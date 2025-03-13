70dea2f4575fde35ccfff345673dd55323f3cc2e
VicenteSK <88107549+VicenteSK@users.noreply.github.com>
2025-03-12 22:52:56 -0300

    first source updating

---
+++ src/app.py
@@ -0,0 +1,50 @@
+from flask import Flask
+from flask_mysqldb import MySQL
+
+from config import config
+
+app = Flask(__name__)
+
+conexion=MySQL(app)
+
+
+@app.route('/cursos')
+def listar_cursos():
+    try:
+        cursor=conexion.connection.cursor()
+        sql="SELECT codigo, nombre, creditos FROM curso"      
+        cursor.execute(sql)  
+        datos=cursor.fetchall()
+        print(datos)
+        return"Cursos listados"
+    except Exception as ex:
+        return "Error"
+
+
+def pagina_no_encontrada(error):
+    return""" 
+    <html>
+        <head>
+            <style>
+                h1 {
+                    color: blue;
+                }
+                p {
+                    font-size: 18px;
+                    color: #333;
+                }
+            </style>
+        </head>
+        <body>
+            <h1>Error 404 - Página No Encontrada</h1>
+            <p>Lo sentimos, la página que estás buscando no existe o ha sido movida.  
+            Verifica la URL e intenta nuevamente.</p>
+        </body>
+    </html>
+    """, 404
+
+
+if __name__ == '__main__':
+    app.config.from_object(config['development'])
+    app.register_error_handler(404,pagina_no_encontrada)
+    app.run()
---
+++ src/config.py
@@ -0,0 +1,11 @@
+class developmentConfig():
+    DEBUG=True
+    MYSQL_HOST ='Localhost'
+    MYSQL_USER ='root'
+    MYSQL_PASSWORD =''
+    MYSQL_DB ='api_flask'
+
+
+config = {
+        'development': developmentConfig
+    }
